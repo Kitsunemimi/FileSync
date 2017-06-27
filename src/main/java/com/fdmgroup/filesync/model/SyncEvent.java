e1a9be381a001fa44cba29e448686baf91fd463a
Harris.Fok <harris.fok@fdmgroup.com>
2017-06-27 10:38:33 -0400

    Add new SyncEvent class

---
+++ src/main/java/com/fdmgroup/filesync/model/SyncEvent.java
@@ -0,0 +1,81 @@
+package com.fdmgroup.filesync.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ * This class is used to identify and track sync events between all directory
+ * pairs. Necessary for finding old states when calculating new changes.
+ * 
+ * @author Harris.Fok
+ */
+@Entity
+@Table(name = "SYNC_EVENT")
+public class SyncEvent {
+	
+	@Id
+	@Column(name = "SYNC_EVENT_ID")
+	@SequenceGenerator(name = "seSeq", sequenceName = "SYNC_EVENT_SEQ", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seSeq")
+	private int id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "STATE_ID_1")
+	private State s1;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "STATE_ID_2")
+	private State s2;
+	
+	@Column(name = "SYNC_TIME")
+	private long date;
+	
+	
+	// Constructor
+	public SyncEvent() {
+		super();
+	}
+	
+	public SyncEvent(State s1, State s2) {
+		this.s1 = s1;
+		this.s2 = s2;
+		
+		date = new Date().getTime();
+	}
+	
+
+	// Getters/setters
+	public State getS1() {
+		return s1;
+	}
+
+	public void setS1(State s1) {
+		this.s1 = s1;
+	}
+
+	public State getS2() {
+		return s2;
+	}
+
+	public void setS2(State s2) {
+		this.s2 = s2;
+	}
+
+	public long getDate() {
+		return date;
+	}
+
+	public void setDate(long date) {
+		this.date = date;
+	}
+}
